71bc6b33b4201520cf055cf99e74b3a4c8ea38d1
Charlie Shi <CharlieS@live.com>
2017-03-05 16:45:17 -0500

    parallax examples commit

---
+++ server/api.js
@@ -0,0 +1,12 @@
+'use strict'; // eslint-disable-line semi
+
+require('APP/db')
+const api = module.exports = require('express').Router() // eslint-disable-line new-cap
+
+api
+  .get('/heartbeat', (req, res) => res.send({ok: true}))
+  .use('/auth', require('./auth'))
+  .use('/users', require('./users'))
+
+// No routes matched? 404.
+api.use((req, res) => res.status(404).end())
